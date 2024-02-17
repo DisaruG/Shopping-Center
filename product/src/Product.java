6e2c759b80d9cd207dfb5a0213b76d628425524b
Disaru Gunawardhana <disaru1314@gmail.com>
2024-02-17 22:45:07 +0530

    pushed

--- product/src/Product.java
+++ product/src/Product.java
@@ -5,4 +5,16 @@ abstract class Product implements Serializable {
     private String productName;
     private int availableQty;
     private double price;
+
+    public Product (String productID, String productName, int availableQty, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.availableQty = availableQty;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getProductID() {
+        return productID;
+    }
 }
