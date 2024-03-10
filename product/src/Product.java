8b196fd4f3d06e3f0cb668da2c82d7dc34ca2f77
Disaru Gunawardhana <disaru1314@gmail.com>
2024-03-10 13:15:35 +0530

    updates

--- product/src/Product.java
+++ product/src/Product.java
@@ -45,4 +45,51 @@ abstract class Product implements Serializable {
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public abstract void displayDetails();
+}
+
+class Electronics extends Product implements Serializable { // Using Serializable indicate that instances of these classes can be serialized.
+    private String brand;
+    private int warranty;
+
+    public Electronics(String productID, String productName, int availableQty, double price, String brand, int warranty) {
+        super(productID, productName, availableQty, price);
+        this.brand = brand;
+        this.warranty = warranty;
+    }
+
+    // Getters and Setters for Electronics-specific attributes
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(int warranty) {
+        this.warranty = warranty;
+    }
+
+    public String toString() {
+        return super.toString() +
+                "\nBrand: " + brand +
+                "\nWarranty Period: " + warranty + "months";
+    }
+
+    public void displayDetails() {
+        System.out.println("Clothing Details: ");
+        System.out.println("Product ID: " + getProductID());
+        System.out.println("ProductName: " + getProductName());
+        System.out.println("Available Quantity: " + getAvailableQty());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Brand: " + getBrand());
+        System.out.println("Warranty: " + getWarranty() + "months");
+        System.out.println("--------------------------------------------");
+    }
 }
